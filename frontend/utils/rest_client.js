75b583b4bd590dc2787d86b2595a47c9274d115d
ilma200 <ilma.sljivo@stu.ibu.edu.ba>
2025-05-22 21:15:25 +0200

    middleware

--- frontend/utils/rest_client.js
+++ frontend/utils/rest_client.js
@@ -1,8 +1,12 @@
 var RestClient = {
     get: function (url, callback, error_callback) {
+        let token = localStorage.getItem("token");
         $.ajax({
             url: Constants.API_BASE_URL + url,
             type: "GET",
+            headers: {
+                Authentication: token,
+            },
             success: function (response) {
                 if (callback) callback(response);
             },
@@ -12,15 +16,19 @@ var RestClient = {
         });
     },
     request: function (url, method, data, callback, error_callback) {
+        let token = localStorage.getItem("token");
         $.ajax({
             url: Constants.API_BASE_URL + url,
             type: method,
+            headers: {
+                Authentication: token,
+            },
             data: data,
         })
             .done(function (response, status, jqXHR) {
                 if (callback) callback(response);
             })
-            .error(function (jqXHR, textStatus, errorThrown) {
+            .fail(function (jqXHR, textStatus, errorThrown) {
                 if (error_callback) {
                     error_callback(jqXHR);
                 } else {
@@ -29,12 +37,19 @@ var RestClient = {
             });
     },
     post: function (url, data, callback, error_callback) {
+        // method used for creating a new entity
         RestClient.request(url, "POST", data, callback, error_callback);
     },
     delete: function (url, data, callback, error_callback) {
+        // method used for deleting an entity
         RestClient.request(url, "DELETE", data, callback, error_callback);
     },
     put: function (url, data, callback, error_callback) {
+        //  method used for updating an entity
         RestClient.request(url, "PUT", data, callback, error_callback);
+    },
+    patch: function (url, data, callback, error_callback) {
+        // method used for updating an entity
+        RestClient.request(url, "PATCH", data, callback, error_callback);
     },
 };
